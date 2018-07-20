e1c6acca50c36159b890de4b4f384fa242d2e3ba
christinacarlson <christina.carlson213@gmail.com>
2018-07-20 13:29:19 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,11 @@
 puts "Let's play FIZZBUZZ!"
 puts "Type a number to see what to say."
+
 num = gets.strip
 fb = num.to_i
-if fb%15 == 0
-  puts "FIZZBUZZ"
+
+  if fb%15 == 0
+    puts "FIZZBUZZ"
 elsif fb%3 == 0 
   puts "FIZZ"
 elsif fb%5 == 0 
