f2a11e00740b7b3eadc89bed67a7aed4f1c98516
christinacarlson <christina.carlson213@gmail.com>
2018-07-20 13:30:08 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,11 +5,11 @@ num = gets.strip
 fb = num.to_i
 
   if fb%15 == 0
-    puts "FIZZBUZZ"
-elsif fb%3 == 0 
-  puts "FIZZ"
-elsif fb%5 == 0 
-  puts "BUZZ"
-else
-  puts fb 
+     puts "FIZZBUZZ"
+  elsif fb%3 == 0 
+     puts "FIZZ"
+  elsif fb%5 == 0 
+     puts "BUZZ"
+  else
+     puts fb 
 end
